5bbbf2932b16869f3f620eb35bc75be08c38f580
Edy Espinal <edyespinal@gmail.com>
2023-10-19 13:13:01 +0200

    Remove unused route getUserProfile

--- src/api/users/controllers/getUserProfile.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express'
-
-export async function getUserProfile(req: Request, res: Response) {
-  try {
-    res.status(200).json({ message: 'getUserProfile' })
-  } catch (error) {
-    res.sendStatus(500)
-  }
-}
