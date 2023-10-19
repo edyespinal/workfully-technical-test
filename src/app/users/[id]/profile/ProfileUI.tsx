69126991ce15a0bfc1dea2854c2fd64db1dd4127
Edy Espinal <edyespinal@gmail.com>
2023-10-19 15:08:41 +0200

    add Profile page

--- src/app/users/[id]/profile/ProfileUI.tsx
+++ src/app/users/[id]/profile/ProfileUI.tsx
@@ -1,28 +1,50 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+'use client'
+
+import { useRouter } from 'next/navigation'
 import { UserCheck2Icon } from 'lucide-react'
 
-function ProfileUI() {
+import { Button } from '@/components/ui/button'
+import { useSession } from '@/hooks/useSession'
+import { User } from '@/schemas/user'
+
+type Props = {
+  user: User
+}
+
+function ProfileUI(props: Props) {
+  const {
+    user: { username, email, name, lastName },
+  } = props
+
+  const router = useRouter()
+  const { logout } = useSession()
+
+  const handleLogout = async () => {
+    await logout()
+
+    router.push('/auth/login')
+  }
+
   return (
-    <div className='flex flex-col gap-2 items-start'>
-      <h1 className='font-bold text-2xl mb-4'>My Profile</h1>
+    <div className="flex flex-col items-start gap-2">
+      <h1 className="mb-4 text-2xl font-bold">My Profile</h1>
       <UserCheck2Icon size={64} />
-      <p className='text-slate-600 mb-4'>
-        You&apos;re logged in as <span className='font-bold'>username</span>
+      <p className="mb-4 text-slate-600">
+        You&apos;re logged in as <span className="font-bold">{username}</span>
       </p>
 
-      <div className='grid grid-cols-3 w-96 mb-16'>
+      <div className="mb-16 grid w-96 grid-cols-3">
         <p>Name</p>
-        <span className='col-span-2 font-bold'>name</span>
+        <span className="col-span-2 font-bold">{name}</span>
 
         <p>Last name</p>
-        <span className='col-span-2 font-bold'>lastname</span>
+        <span className="col-span-2 font-bold">{lastName}</span>
 
         <p>Email</p>
-        <span className='col-span-2 font-bold'>email</span>
+        <span className="col-span-2 font-bold">{email}</span>
       </div>
 
-      <Button>Log out</Button>
+      <Button onClick={handleLogout}>Log out</Button>
     </div>
   )
 }
--- src/app/users/[id]/profile/page.tsx
+++ src/app/users/[id]/profile/page.tsx
@@ -1,7 +1,44 @@
+import { request } from '@/utils/fetch'
 import { ProfileUI } from './ProfileUI'
+import { cookies } from 'next/headers'
+import { ShieldBan } from 'lucide-react'
+import { User } from '@/schemas/user'
 
-function ProfilePage() {
-  return <ProfileUI />
+type Props = {
+  params: { id: string }
+}
+
+async function ProfilePage(props: Props) {
+  const { params } = props
+
+  let user: User | null = null
+
+  try {
+    const response = await request(`/api/users/${params.id}/profile`, {
+      headers: {
+        Cookie: cookies()
+          .getAll()
+          .map(({ name, value }) => `${name}=${value}`)
+          .join('; '),
+      },
+    })
+
+    user = response
+  } catch (error) {
+    console.log(error)
+  }
+
+  if (!user) {
+    return (
+      <div className="py-8">
+        <ShieldBan size={64} />
+        <h1 className="mt-4 text-2xl font-bold">Unauthorized</h1>
+        <p>Sorry, but you cannot access this information</p>
+      </div>
+    )
+  }
+
+  return <ProfileUI user={user} />
 }
 
 export default ProfilePage
